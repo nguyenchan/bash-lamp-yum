6c9f652832d3299936a37889b7fa960fc1f8d073
NguyenChan <pkcnguyen@gmail.com>
2018-04-09 16:35:37 +0700

    ver1.......

--- lamp.sh
+++ lamp.sh
@@ -22,7 +22,7 @@ yum -y install mysql mysql-server
 chkconfig mysqld on
 service mysqld start
 
-$DATABASE_PASS = "nguyenchan"
+DATABASE_PASS = "nguyenchan"
 mysqladmin -u root password "$DATABASE_PASS"
 mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_PASS') WHERE User='root'"
 mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
