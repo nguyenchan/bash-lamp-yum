216a38877ccf7aecfbc82a708ef0b1c3eaa0a155
NguyenChan <pkcnguyen@gmail.com>
2018-04-09 16:41:09 +0700

    ver1........

--- lamp.sh
+++ lamp.sh
@@ -22,13 +22,16 @@ yum -y install mysql mysql-server
 chkconfig mysqld on
 service mysqld start
 
-DATABASE_PASS = "nguyenchan"
-mysqladmin -u root password "$DATABASE_PASS"
-mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_PASS') WHERE User='root'"
-mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
-mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
-mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
-mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
+mysql_secure_installation <<EOF
+y
+somepass
+somepass
+y
+y
+y
+y
+y
+EOF
 
 
 echo "Start Installing PHP..."
